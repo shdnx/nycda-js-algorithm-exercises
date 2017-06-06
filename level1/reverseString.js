cfef4270d4d231989774abb193754964b97b4cdf
Gábor Kozár <id@gaborkozar.me>
2017-06-06 16:11:43 +0200

    New exercise: reverseString

---
+++ level1/reverseString.js
@@ -0,0 +1,29 @@
+/*
+ * Given a string, returns a new string that is the same as the original string with the order of its characters reversed.
+ * You're not allowed to use String.prototype.reverse().
+ * Returns the new string.
+ */
+function reverseString() {
+
+}
+
+describe('reverseString', function() {
+  const assert = require("chai").assert;
+
+  it('should work for empty string', function() {
+    assert.equal("", reverseString(""));
+  });
+
+  it('should work for single-character strings', function() {
+    assert.equal("a", reverseString("a"));
+    assert.equal(" ", reverseString(" "));
+    assert.equal("*", reverseString("*"));
+  });
+
+  it('should work on non-trivial strings', function() {
+    assert.equal("abc", reverseString("cba"));
+    assert.equal("raboof", reverseString("foobar"));
+    assert.equal("potato", reverseString("otatop"));
+    assert.equal("foof", reverseString("foof"));
+  });
+});
