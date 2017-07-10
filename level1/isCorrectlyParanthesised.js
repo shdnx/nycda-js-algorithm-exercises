4679caba7477b5e799c631e82682a84b05be60ae
Gabor Kozar <id@gaborkozar.me>
2017-07-10 20:00:38 +0200

    Added missing testcase for isCorrectlyParanthesised

--- level1/isCorrectlyParanthesised.js
+++ level1/isCorrectlyParanthesised.js
@@ -27,7 +27,10 @@ describe('isCorrectlyParanthesised', function() {
 
   it('should detect incorrectly paranthesised text', function() {
     assert.isFalse(isCorrectlyParanthesised("("));
+    assert.isFalse(isCorrectlyParanthesised(")("));
     assert.isFalse(isCorrectlyParanthesised("(()"));
+    assert.isFalse(isCorrectlyParanthesised("))(("));
+    assert.isFalse(isCorrectlyParanthesised(")(()"));
     assert.isFalse(isCorrectlyParanthesised("(((("));
     assert.isFalse(isCorrectlyParanthesised("))()"));
     assert.isFalse(isCorrectlyParanthesised("(Test("));
